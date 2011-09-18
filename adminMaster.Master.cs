19471dcb9588173ca8eb168b80b2ba8ad772b7b7
NKK <nkk@gmail.com>
2011-09-18 11:31:27 +0530

    AdminMaster: Created top level master page for admin
    New admin page added. This page is top level admin page and all pages
    should be drived from this page.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ adminMaster.Master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EC.UI;
+using EC.BL;
+//using EC.BL.Providers.Announcements;
+using EC.Common;
+using EC.Model;
+using EC.Common.Utilities;
+using EC.BL.Providers.User;
+
+public partial class adminMaster : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblusername.Text = "Welcome Admin:&nbsp;" + UserIdentity.AdminUsername;
+    }
+}
+
