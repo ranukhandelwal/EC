5d8051f095f8c805baaecb1ecf79feb3e5d46413
NKK <nkk@gmail.com>
2011-09-29 18:56:18 +0530

    Master: Header Footer: Content div correction
    In content div with class 3of4 were applied twice. Corrected
    Signed-off-by: NKK <nkk@gmail.com>

--- HeaderFooter.Master.designer.cs
+++ HeaderFooter.Master.designer.cs
@@ -32,6 +32,15 @@ namespace ExamCrazy {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// AddToBreadCrum control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder AddToBreadCrum;
+        
+        /// <summary>
         /// LeftPanelHolder control.
         /// </summary>
         /// <remarks>
