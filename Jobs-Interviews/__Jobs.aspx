f4190d32a29558b09c022a2db2ca9e1de676f44a
NKK <nkk@gmail.com>
2011-10-01 13:10:54 +0530

    Jobs-Interview: Added base files
    Base files for job pages, will be called through url rewrite
    
    	new file:   Jobs-Interviews/__Jobs.aspx
    	new file:   Jobs-Interviews/__Jobs.aspx.cs
    	new file:   Jobs-Interviews/__Jobs.aspx.designer.cs
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ Jobs-Interviews/__Jobs.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Job.master" AutoEventWireup="true"
+    CodeBehind="__Jobs.aspx.cs" Inherits="__Jobs" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="AddToBreadCrum" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jobDetail unit">
+        <h3>
+            <asp:Label id="lblTitle" runat="server"/>
+        </h3>
+    </div>
+    <ul>
+        <li>
+            <h5>
+                JOB Summary</h5>
+        </li>
+    </ul>
+</asp:Content>
---
+++ Jobs-Interviews/__Jobs.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EC.UI;
+using EC.BL;
+using EC.BL.Providers;
+using EC.Common;
+using EC.Model;
+using EC.Common.Utilities;
+using EC.Security;
+
+
+public partial class __Jobs : BasePage
+{
+    static string RequestedPage;
+    JobRepository job = new JobRepository();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            
+
+            RequestedPage = Request.QueryString["RequestedURL"];
+            int jobid = job.isValidJob("ExamCrazy.com/jobsExamCrazy_Vacancy.asp", 2);
+            if (jobid > 0)
+            {
+                job.FillUp(jobid);
+            }
+        }
+        lblTitle.Text = job.Title;
+
+    }
+}
+
