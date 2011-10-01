bb80fecf9540da5664f9e506479dfa506c3605ad
NKK <nkk@gmail.com>
2011-10-01 13:08:56 +0530

    Job: Added model and providers
    Add files for model and repository.
    
    moldel is inherited from feed model and repository in providers is
    inherited from feed repository.
    
    No major work is done today but they are required considering future
    expansion
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ App_Code/BLL/Providers/Jobs/JobRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using EC.Model;
+using EC.Common;
+using EC.BL;
+using EC.Common.Utilities;
+
+namespace EC.BL
+{
+    public class JobRepository : FeedRepository
+    {
+        
+        public JobRepository()
+        {
+            foreach (sCategory c in constant.FeedCategory)
+                if (c.Name == "JOBS")
+                    CategoryID = c.ID;
+            
+        }
+
+        public int Add(Job j)
+        {
+
+            return base.Add(j);
+        }
+
+        public int Update(Job j)
+        {
+            return base.Update(j);
+        }
+
+
+        /// <summary>
+        /// Is valid feed, checks with respect to link and category
+        /// </summary>
+        public int isValidJob(string link, int State)
+        {
+            return base.isValidFeed(link, CategoryID, State);
+        }
+
+
+        /// <summary>
+        /// Get Job title, author, date, hits, summmary, description etc from job id
+        /// </summary>
+        public override void FillUp(int ID)
+        {
+            
+            try
+            {
+                base.FillUp(ID);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static Job ConvertFeedToJob(Feed f)
+        {
+            return (Job)f;
+        }
+
+        public ExtendedCollection<Job> GetJobList()
+        {
+            ExtendedCollection<Job> jlist = new ExtendedCollection<Job>();
+            jlist.ConvertAll(base.GetFeedList(CategoryID), new Converter<Feed, Job>(ConvertFeedToJob));
+            return jlist;
+        }
+        
+    }
+}
---
+++ App_Code/Model/Job.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using EC.Model;
+using EC.Common;
+using EC.BL;
+
+namespace EC.Model
+{
+    public class Job: Feed
+    {
+        public Job()
+        {
+            
+        }
+
+        public Job(Feed f)
+        {
+            this.Author = f.Author;
+            this.CategoryID = f.CategoryID;
+            this.CategorySTR = f.CategorySTR;
+            this.DatePublised = f.DatePublised;
+            this.Description = f.Description;
+            this.DisplayIn = f.DisplayIn;
+            this.FeedID = f.FeedID;
+            this.isValid = f.isValid;
+            this.Link = f.Link;
+            this.Summary = f.Summary;
+            this.Title = f.Title;
+            for (int i; this.Hits < f.Hits; )
+                i = this.IncrHit;
+        }
+    }
+}
