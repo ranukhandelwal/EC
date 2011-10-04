a4625363b7a22bfe1f9eb2ad34937733cce90902
NKK <nkk@gmail.com>
2011-10-04 19:46:17 +0530

    Master: HeaderFooter: Update: From DataBase
    Added update support from database.
    
    TBD: this should pick as per displayin field for feed table.
    
    Signed-off-by: NKK <nkk@gmail.com>

--- HeaderFooter.Master.cs
+++ HeaderFooter.Master.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using EC.BL;
+using EC.Model;
+using EC.Common;
 
 namespace ExamCrazy
 {
@@ -11,6 +14,17 @@ namespace ExamCrazy
         protected void Page_Load(object sender, EventArgs e)
         {
             Page.Header.DataBind();
+            if (!Page.IsPostBack)
+            {
+                UpdateRepository UpdateList = new UpdateRepository();
+                ExtendedCollection<Update> UC = UpdateList.GetUpdateList();
+
+                Update[] updates = new Update[UC.Count];
+                UC.CopyTo(updates, 0);
+                RepeaterID.DataSource = updates;
+                RepeaterID.DataBind();
+
+            }
 
         }
     }
--- HeaderFooter.Master.designer.cs
+++ HeaderFooter.Master.designer.cs
@@ -50,6 +50,15 @@ namespace ExamCrazy {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder LeftPanelHolder;
         
         /// <summary>
+        /// RepeaterID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater RepeaterID;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
