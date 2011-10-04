7d01d8aa2b6824658979bee7e7d85e7ed79b3b1a
NKK <nkk@gmail.com>
2011-10-04 19:35:52 +0530

    Update: Model + Provider
    Added Model and Provider for Update
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ App_Code/BLL/Providers/Update/UpdateRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Collections.Generic;
+using EC.Model;
+using EC.Common;
+using EC.BL;
+using EC.Common.Utilities;
+
+namespace EC.BL
+{
+    public class UpdateRepository : FeedRepository
+    {
+
+        public UpdateRepository()
+        {
+            foreach (sCategory c in constant.FeedCategory)
+                if (c.Name == "UPDATE")
+                    CategoryID = c.ID;
+
+        }
+
+        public int Add(Update u)
+        {
+
+            return base.Add(u);
+        }
+
+        public int Update(Update u)
+        {
+            return base.Update(u);
+        }
+
+
+        
+        /// <summary>
+        /// Get Update title, author, date, hits, summmary, description etc from Update id
+        /// </summary>
+        public override void FillUp(int ID)
+        {
+
+            try
+            {
+                base.FillUp(ID);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static Update ConvertFeedToUpdate(Feed f)
+        {
+            Update u = new Update(f);
+            return u;
+        }
+
+        public ExtendedCollection<Update> GetUpdateList()
+        {
+            ExtendedCollection<Update> ulist = new ExtendedCollection<Update>();
+            IEnumerable<Update> list = ulist.ConvertAll(base.GetFeedList(CategoryID), new Converter<Feed, Update>(ConvertFeedToUpdate));
+            ulist.AddRange(list);
+            return ulist;
+        }
+
+    }
+}
