a906788548a13dedfa10b59f08252a47d558387a
NKK <nkk@gmail.com>
2011-10-25 22:57:09 +0530

    Control: User Info: Added user info control
    Added user info control. This takes link names from Xml userinfo.xml
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ Control/UserInfo.ascx
@@ -0,0 +1,26 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UserInfo.ascx.cs" Inherits="ExamCrazy.Control.UserInfo" %>
+<div class="userInfo unit">
+    <h2>
+        Welcome Examcrazy user</h2>
+    <p>
+        examcrazy@examcrazy.com</p>
+    <div class="size1of2 unit">
+        <a href="#" title="User Wall">
+            <img src="~/img/userwall.gif" alt="User wall" title="User Wall" runat="server" /></a>
+    </div>
+    
+    <asp:Repeater ID="userinfo" runat="server">
+        <HeaderTemplate>
+            <div class="lastUnit">
+                <ul>
+        </HeaderTemplate>
+        <ItemTemplate>
+            <li><a id="A1" href="<%#Page.ResolveUrl(((System.Xml.XmlNode)Container.DataItem).Attributes["link"].InnerText)%>"
+                runat="server">
+                <%#((System.Xml.XmlNode)Container.DataItem).Attributes["name"].InnerText%></a>
+        </ItemTemplate>
+        <FooterTemplate>
+            </ul> </div>
+        </FooterTemplate>
+    </asp:Repeater>
+</div>
---
+++ Control/UserInfo.ascx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Data;
+using System.Xml;
+using System.Text;
+using EC.BL;
+
+namespace ExamCrazy.Control
+{
+    public partial class UserInfo : System.Web.UI.UserControl
+    {
+        public string UserInfoXML = "~/XML/UserInfo.xml";
+        public string filter;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlLink csslink = new HtmlLink();
+            if (Page.Header.FindControl("userinfo") == null)
+            {
+                csslink.ID = "cssreciaspx";
+                csslink.Attributes.Add("rel", "stylesheet");
+                csslink.Attributes.Add("type", "text/css");
+                csslink.Href = "CSS/userinfo.css";
+                Page.Header.Controls.Add(csslink);
+            }
+
+            filter = SiteConfiguration.GetUserInfoFilter("ajaykh");
+            if (!Page.IsPostBack)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(Server.MapPath(UserInfoXML));
+                XmlNodeList nodes = doc.SelectNodes("UserInfo/list[contains(@displayin,'" + filter + "')]");
+
+                userinfo.DataSource = nodes;
+                userinfo.DataBind();
+
+
+            }
+
+        }
+    }
+}
