2fd618783df884c595f1d097443e458975fd2d42
NKK <nkk@gmail.com>
2011-12-27 14:05:09 +0530

    Admin: Add Article: Pages to add Article Category
    These pages define top level pages for adding article category in the
    Article_Category table.
    New Article Category can be added or existing can be edited.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ admin/AddArticle.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminArticle.master" AutoEventWireup="true"
+    CodeBehind="AddArticle.aspx.cs" Inherits="ExamCrazy.admin.AddArticle" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="headerquerycss" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PageName" runat="server">
+    Articles
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="AddToBreadCrum" runat="server">
+    <img id="Img1" src="~/images/arrow.gif" align="absmiddle" runat="server" /><a id="A1"
+        href="~/admin/ArticleManager.aspx" class="content12" title="Back to Article manager main page"
+        runat="server">Article Manager Main Page</a>&nbsp;&nbsp;<asp:Label runat="server"
+            ID="lblreset" Visible="false" CssClass="content12" EnableViewState="false" />
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="PageNameBox" runat="server">
+    Add Articles
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Panel ID="Panel2" runat="server" Width="500px">
+        <div style="width: 400px">
+            <div class="nifty">
+                <b class="rtop"><b class="r1"></b><b class="r2"></b><b class="r3"></b><b class="r4">
+                </b></b>
+                <div class="dcntmenu">
+                    <asp:Label ID="lblheaderform" CssClass="contadmin" runat="server" /></div>
+            </div>
+            <div class="contentdisplay3">
+                <div class="contentdis5">
+                    <asp:Label ID="lblnamedis2" CssClass="content2" runat="server" />
+                    <asp:TextBox runat="server" ID="CategoryName" CssClass="textbox" Width="200px" maxlenght="18" />
+                    <br />
+                    <div class="content2">
+                        Description:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:TextBox runat="server" ID="CategoryDesc" CssClass="textbox" Width="200px" maxlenght="200" />
+                        <br />
+                        IsActive:
+                        <asp:CheckBox runat="server" ID="CheckisActive" />
+                    </div>
+                    <asp:Button runat="server" Text="Update" ID="updatebutton" ToolTip="Click to update"
+                        CssClass="submitadmin" align="center" OnClick="Update_Category" />
+                    <asp:Button runat="server" Text="Add" ToolTip="Click to add new category" ID="addbutton"
+                        CssClass="submitadmin" align="center" OnClick="Add_Category" />
+                    <input type="text" runat="server" id="CategoryID" name="CategoryID" class="textbox"
+                        size="2" maxlenght="2" readonly="True" style="visibility: hidden;" />
+                    <br />
+                    <asp:Panel ID="Panel3" runat="server">
+                        <div style="padding-top: 4px; padding-bottom: 6px;">
+                            <span class="content2"><span class="cred">Are you sure you want to delete this category?
+                                <br />
+                                Note: all article belong to this category will be deleted as well. </span>
+                                <br />
+                                <asp:Button runat="server" Text="Delete" ToolTip="Click to delete category and its associated recipes"
+                                    ID="DelCategory" CssClass="submitadmin" />
+                            </span>
+                        </div>
+                    </asp:Panel>
+                </div>
+            </div>
+            <br />
+        </div>
+    </asp:Panel>
+    <fieldset>
+        <legend>Available Article Category</legend>
+        <div style="margin-bottom: 5px;">
+            <span class="content12">Edit/Delete a category</span>
+        </div>
+        <asp:Repeater ID="ArtCategoryList" runat="server" OnItemDataBound="ArtCategoryList_ItemDataBound"
+            OnItemCommand="Delete_Category">
+            <ItemTemplate>
+                <div class="dcnt2">
+                    <asp:HyperLink ID="editbutton2" NavigateUrl='<%# "addarticle.aspx?editcatid=" + Eval("CAT_ID")%>'
+                        CssClass="content2" runat="server" />&nbsp;&nbsp;<asp:LinkButton ID="delbutton2"
+                            CssClass="content2" runat="server" CommandName='Delete' CommandArgument='<%# Eval("CAT_ID") %>' />&nbsp;&nbsp;<span
+                                class="catart1"><%# Eval("CAT_NAME") %>
+                                (<%# Eval("REC_COUNT") %>)</span>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </fieldset>
+    <asp:Literal ID="JSLiteral" runat="server"></asp:Literal>
+</asp:Content>
---
+++ admin/AddArticle.aspx.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using EC.Common;
+using EC.Common.Utilities;
+using EC.Model;
+using EC.BL;
+using EC.BL.Providers.Article;
+
+namespace ExamCrazy.admin
+{
+    public partial class AddArticle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DelCategory.Visible = false;
+                CategoryName.Text = "";
+                CategoryID.Visible = false;
+                Panel2.Visible = true;
+                Panel3.Visible = false;
+                addbutton.Visible = true;
+                updatebutton.Visible = false;
+                lblheaderform.Text = "Add New Article Category";
+                lblnamedis2.Text = "Category Name:";
+                
+                //Return Add New Category List
+
+                ArtCategoryList.DataSource = Blogic.ActionProcedureDataProvider.GetArticleCategoryList;
+                ArtCategoryList.DataBind();
+
+                if (Request.QueryString["editcatid"] != null)
+                {
+                    CategoryID.Value = Request.QueryString["editcatid"];
+                    IDataReader oDr = Blogic.ActionProcedureDataProvider.GetArticleCategoryDetails(Int32.Parse(CategoryID.Value));
+                    oDr.Read();
+                    CategoryName.Text = oDr.GetValue(oDr.GetOrdinal("CAT_NAME")).ToString();
+                    CategoryDesc.Text = oDr.GetValue(oDr.GetOrdinal("CAT_DESC")).ToString();
+                    Panel2.Visible = true;
+                    if (Int32.Parse(oDr.GetValue(oDr.GetOrdinal("isActive")).ToString()) == 1)
+                    {
+                        CheckisActive.Checked = true;
+                    }
+                    Panel3.Visible = false;
+                    addbutton.Visible = false;
+                    updatebutton.Visible = true;
+                    lblheaderform.Text = "Updating Article Category ID# " + Request.QueryString["editcatid"];
+                }
+            }
+
+        }
+
+        public void ArtCategoryList_ItemDataBound(Object s, RepeaterItemEventArgs e)
+        {
+            // This event is raised for the header, the footer, separators, and items.
+            // Execute the following logic for Items and Alternating Items.
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                LinkButton delbutton2 = (LinkButton)(e.Item.FindControl("delbutton2"));
+                HyperLink editbutton2 = (HyperLink)(e.Item.FindControl("editbutton2"));
+
+                delbutton2.Attributes["onclick"] = "javascript:return confirm('Are you sure you want to delete this category and all its associated articles. Note: you will loss all articles belong in this category. " + DataBinder.Eval(e.Item.DataItem, "CAT_NAME") + " Category, ID#  " + DataBinder.Eval(e.Item.DataItem, "CAT_ID") + "?')";
+                delbutton2.Text = "<img border='0' src='../images/icon_delete.gif'>";
+                editbutton2.Text = "<img src='../images/icon_pencil.gif' alt='Edit' border='0'>";
+                editbutton2.Attributes.Add("onmouseover", "Tip('Edit <b>" + DataBinder.Eval(e.Item.DataItem, "CAT_NAME") + "</b> category.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                editbutton2.Attributes.Add("onmouseout", "UnTip()");
+                delbutton2.Attributes.Add("onmouseover", "Tip('Delete (<b>" + DataBinder.Eval(e.Item.DataItem, "CAT_NAME") + "</b>) category.<br><b>Note: </b> deleting this category will delete<br>all articles belong to this category.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                delbutton2.Attributes.Add("onmouseout", "UnTip()");
+                Panel3.Visible = false;
+            }
+        }
+
+        //Handles update comment
+        public void Update_Category(Object s, EventArgs e)
+        {
+            ArticleCategoryRepository Category = new ArticleCategoryRepository();
+
+            Category.CatID = Int32.Parse(Request.QueryString["editcatid"]);
+            Category.Category = Request.Form[CategoryName.UniqueID];
+            Category.CategoryDescription = Request.Form[CategoryDesc.UniqueID];
+            if (CheckisActive.Checked == true)
+                Category.isActive = 1;
+            else
+                Category.isActive = 0;
+
+            Caching.PurgeCacheItems("ArticleCategory_SideMenu");
+
+            //Notify user if error occured.
+            if (Category.Update(Category) != 0)
+            {
+                JSLiteral.Text = "Error occured while processing your submit.";
+                return;
+            }
+
+            Category = null;
+
+            Response.Redirect("confirmarticlecatedit.aspx?catname=" + Request.Form["CategoryName"] + "&mode=update");
+        }
+
+        //Handle the delete button click event
+        public void Delete_Category(Object sender, RepeaterCommandEventArgs e)
+        {
+            if ((e.CommandName == "Delete"))
+            {
+                ArticleRepository Category = new ArticleRepository();
+
+                Category.CatID = Convert.ToInt32(e.CommandArgument);
+
+                Caching.PurgeCacheItems("ArticleCategory_SideMenu");
+
+                //Perform delete
+                Category.DeactivateCategory(Category);
+
+                //Redirect to confirm delete page
+                Response.Redirect("confirmarticlecatedit.aspx?catname=ArticleCategoryID" + Category.CatID + "&mode=del");
+            }
+        }
+
+        //Handle add new category
+        public void Add_Category(Object s, EventArgs e)
+        {
+            ArticleCategoryRepository Category = new ArticleCategoryRepository();
+
+            Category.Category = Request.Form[CategoryName.UniqueID];
+            Category.CategoryDescription = Request.Form[CategoryDesc.UniqueID]; 
+
+            Caching.PurgeCacheItems("ArticleCategory_SideMenu");
+
+            //Notify user if error occured.
+            if (Category.Add(Category) != 0)
+            {
+                JSLiteral.Text = "Error occured while processing your submit.";
+                return;
+            }
+
+            Category = null;
+
+            Response.Redirect("confirmarticlecatedit.aspx?catname=" + Request.Form["CategoryName"] + "&mode=add");
+        }
+
+        public void ArticleCat_ItemDataBound(Object s, RepeaterItemEventArgs e)
+        {
+            // This event is raised for the header, the footer, separators, and items.
+            // Execute the following logic for Items and Alternating Items.
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                LinkButton delbutton3 = (LinkButton)(e.Item.FindControl("delbutton3"));
+                HyperLink editbutton3 = (HyperLink)(e.Item.FindControl("editbutton3"));
+                delbutton3.Text = "<img border='0' src='../images/icon_delete.gif'>";
+                editbutton3.Text = "<img src='../images/icon_pencil.gif' alt='Edit' border='0'>";
+                editbutton3.NavigateUrl = "updatearticle.aspx?aid=" + DataBinder.Eval(e.Item.DataItem, "ID");
+                delbutton3.Attributes["onclick"] = "javascript:return confirm('Are you sure you want to delete " + DataBinder.Eval(e.Item.DataItem, "Title") + " Article, ID#  " + DataBinder.Eval(e.Item.DataItem, "ID") + "?')";
+                delbutton3.Attributes.Add("onmouseover", "Tip('Delete (<b>" + DataBinder.Eval(e.Item.DataItem, "Title") + "</b>) article.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                delbutton3.Attributes.Add("onmouseout", "UnTip()");
+                editbutton3.Attributes.Add("onmouseover", "Tip('Edit (<b>" + DataBinder.Eval(e.Item.DataItem, "Title") + "</b>) article.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                editbutton3.Attributes.Add("onmouseout", "UnTip()");
+            }
+        }
+
+    }
+}
