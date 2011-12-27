556758ca709eeb267eee8147d27142926fe0f9a3
NKK <nkk@gmail.com>
2011-12-27 13:58:44 +0530

    Providers: Article Category
    Added provider files for Article Category. They are used as helper
    classes for article category.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ App_Code/BLL/Providers/Article/ArticleCategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using EC.Model;
+
+namespace EC.BL
+{
+    public class ArticleCategoryRepository : BaseArticleCategoryObj
+    {
+       /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public ArticleCategoryRepository()
+        {
+        }
+
+        /// <summary>
+        /// Perform Insert to Database
+        /// </summary>
+        /// <returns></returns>
+        public override int Add(ArticleCategory Category)
+        {
+            return Blogic.ActionProcedureDataProvider.AdminAddNewArticleCategory(Category);
+        }
+
+        /// <summary>
+        /// Perform Update
+        /// </summary>
+        /// <returns></returns>
+        public override int Update(ArticleCategory Category)
+        {
+            return Blogic.ActionProcedureDataProvider.UpdateArticleCategory(Category);
+        }
+
+        /// <summary>
+        /// Perform Deactivate
+        /// </summary>
+        /// <returns></returns>
+        public override int Deactivate(ArticleCategory Category)
+        {
+            return Blogic.ActionProcedureDataProvider.DeactivateArticleCategory(Category);
+        }
+
+       
+    }
+}
---
+++ App_Code/BLL/Providers/Article/BaseArticleCategory.cs
@@ -0,0 +1,32 @@
+﻿#region XD World Recipe V 3
+// FileName: BaseArticleObj.cs
+// Author: Dexter Zafra
+// Date Created: 6/29/2008
+// Website: www.ex-designz.net
+#endregion
+using System;
+using EC.Model;
+
+namespace EC.BL
+{
+    /// <summary>
+    /// Object in this class manages Article CRUD database methods.
+    /// </summary>
+    public abstract class BaseArticleCategoryObj : ArticleCategory, IRepository
+    {
+        #region Class members
+        public virtual int Add(ArticleCategory category) { return 0; } //Insert to database
+        public virtual int Update(ArticleCategory category) { return 0; } //Update to database
+        public virtual int Deactivate(ArticleCategory category) { return 0; } //Deactivate from database
+        public virtual void FillUp(int ID) { } //Fill up database fields
+        #endregion
+
+        #region Interface Contract Implementation - overload methods
+        public virtual int Add() { return 0; } //Insert to database
+        public virtual int Update() { return 0; } //Update to database
+        public virtual int Deactivate() { return 0; } //Deactivate from database
+        public virtual void FillUp() { } //Fill up database fields
+        #endregion
+    }
+}
+
