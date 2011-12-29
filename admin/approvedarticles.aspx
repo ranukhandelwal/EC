f319608c1bfc328d68e0d3b3729fb339d287e92b
NKK <nkk@gmail.com>
2011-12-29 15:18:31 +0530

    Admin: Articles: Approved
    Added support for viewing and updating approved articles
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ admin/approvedarticles.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminArticle.master" AutoEventWireup="true"
+    CodeBehind="approvedarticles.aspx.cs" Inherits="ExamCrazy.admin.approvedarticles" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="headerquerycss" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="PageName" runat="server">
+    Articles
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="AddToBreadCrum" runat="server">
+    <img id="Img1" src="~/images/arrow.gif" align="absmiddle" runat="server" /><a id="A1"
+        href="~/admin/ArticleManager.aspx" class="content12" title="Back to Article manager main page"
+        runat="server">Article Manager Main Page</a>&nbsp;&nbsp;<asp:Label runat="server"
+            ID="lblreset" Visible="false" CssClass="content12" EnableViewState="false" />
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="PageNameBox" runat="server">
+    Approved Articles
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="margin-left: 20px; margin-top: 20px; margin-right: 50px;">
+        <asp:Panel ID="articlelist" runat="server" Width="700px" Visible="false">
+            <fieldset>
+                <legend>
+                    <asp:Label Font-Names="verdana" ID="lbcatname" runat="server" />
+                    Category</legend>
+                <asp:Literal ID="Error" runat="server"></asp:Literal>
+                <asp:Repeater ID="ArticleCat" runat="server" OnItemDataBound="ArticleCat_ItemDataBound"
+                    OnItemCommand="Delete_Article">
+                    <ItemTemplate>
+                        <div class="dcnt2">
+                            <asp:HyperLink ID="editbutton3" CssClass="content2" runat="server" />&nbsp;&nbsp;<asp:LinkButton
+                                ID="delbutton3" CssClass="content2" runat="server" CommandName='Delete' CommandArgument='<%# Eval("ID") %>' />&nbsp;&nbsp;<a
+                                    class="dt" href='javascript:void(0)' onclick="window.open('articlepreview.aspx?aid=<%# Eval("ID") %>','','width=850,height=700,scrollbars=yes,resizable=yes,status=no,left=180,top=180'); return false;"
+                                    onmouseover="Tip('Click to preview article.<br><b>Author: </b><%# Eval("Author") %><br><b>Hits: </b><%# Eval("Hits") %><br><b>Posted on: </b> <%# Eval("Post_Date", "{0:M/d/yyyy}")%>', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')"
+                                    onmouseout="UnTip()"><%# Eval("Title")%></a></span>
+                        </div>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </fieldset>
+        </asp:Panel>
+    </div>
+    <fieldset>
+        <legend>Approved Articles List</legend>
+        <div style="margin-bottom: 5px;">
+            <span class="content12">Edit an Articles<br />
+                Note any article eddited will move to unapproved article list and will not be shown</span>
+        </div>
+        <asp:Repeater ID="ArtCategoryList" runat="server" OnItemDataBound="ArtCategoryList_ItemDataBound">
+            <ItemTemplate>
+                <div class="dcnt2">
+                    <span class="catart1">
+                    <a class="dt" href="approvedarticles.aspx?catid=<%# Eval("CAT_ID") %>" >
+                        <%# Eval("CAT_NAME") %>
+                        (<%# Eval("REC_COUNT") %>)</span></a>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </fieldset>
+</asp:Content>
---
+++ admin/approvedarticles.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using EC.Common;
+using EC.Common.Utilities;
+using EC.Model;
+using EC.BL;
+using EC.BL.Providers.Article;
+using EC.UI;
+
+namespace ExamCrazy.admin
+{
+    public partial class approvedarticles : BasePageAdmin
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //Return Add New Category List
+
+                ArtCategoryList.DataSource = Blogic.ActionProcedureDataProvider.GetArticleCategoryList;
+                ArtCategoryList.DataBind();
+
+                ShowEditArticleListing();
+            }
+        }
+
+        //Handles show edit article listing
+        private void ShowEditArticleListing()
+        {
+            //Instantiate validation
+            Utility Util = new Utility();
+
+            int CatId;
+            CatId = (int)Util.Val(Request.QueryString["catid"]);
+
+            if (string.IsNullOrEmpty(this.Request.QueryString["catid"]))
+            {
+                articlelist.Visible = false;
+            }
+            else
+            {
+                articlelist.Visible = true;
+
+                try
+                {
+                    IDataReader dr = Blogic.ActionProcedureDataProvider.GetArticleCategory(CatId, 3); //3 = Category OrderBy Name.
+
+                    dr.Read();
+                    lbcatname.Text = dr["CAT_NAME"].ToString();
+                    dr.Close();
+
+                    ArticleCat.DataSource = Blogic.ActionProcedureDataProvider.GetArticleCategory(CatId, 3); //3 = Category OrderBy Name.
+                    ArticleCat.DataBind();
+                }
+                catch
+                {
+                    Error.Text = "No Record Found.";
+                    return;
+                }
+
+                //Release allocated memory
+                Util = null;
+            }
+        }
+
+        //Handle the delete button click event
+        public void Delete_Article(Object sender, RepeaterCommandEventArgs e)
+        {
+            if ((e.CommandName == "Delete"))
+            {
+                ArticleRepository DeleteArticle = new ArticleRepository();
+
+                DeleteArticle.ID = Convert.ToInt32(e.CommandArgument);
+
+                Caching.PurgeCacheItems("Newest_Articles");
+                Caching.PurgeCacheItems("ArticleCategory_SideMenu");
+
+                //Perform delete
+                DeleteArticle.Deactivate();
+
+                //Release allocated memory
+                DeleteArticle = null;
+
+                //Redirect to confirm delete page
+                Response.Redirect("articlemanager.aspx");
+            }
+        }
+
+        public void ArtCategoryList_ItemDataBound(Object s, RepeaterItemEventArgs e)
+        {
+            // This event is raised for the header, the footer, separators, and items.
+            // Execute the following logic for Items and Alternating Items.
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                /*LinkButton delbutton2 = (LinkButton)(e.Item.FindControl("delbutton2"));
+                HyperLink editbutton2 = (HyperLink)(e.Item.FindControl("editbutton2"));*/
+
+                /*delbutton2.Attributes["onclick"] = "javascript:return confirm('Are you sure you want to delete this category and all its associated articles. Note: you will loss all articles belong in this category. " + DataBinder.Eval(e.Item.DataItem, "CAT_NAME") + " Category, ID#  " + DataBinder.Eval(e.Item.DataItem, "CAT_ID") + "?')";
+                delbutton2.Text = "<img border='0' src='../images/icon_delete.gif'>";
+                editbutton2.Text = "<img src='../images/icon_pencil.gif' alt='Edit' border='0'>";
+                editbutton2.Attributes.Add("onmouseover", "Tip('Edit <b>" + DataBinder.Eval(e.Item.DataItem, "CAT_NAME") + "</b> category.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                editbutton2.Attributes.Add("onmouseout", "UnTip()");
+                delbutton2.Attributes.Add("onmouseover", "Tip('Delete (<b>" + DataBinder.Eval(e.Item.DataItem, "CAT_NAME") + "</b>) category.<br><b>Note: </b> deleting this category will delete<br>all articles belong to this category.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                delbutton2.Attributes.Add("onmouseout", "UnTip()");*/
+                
+            }
+        }
+
+        public void ArticleCat_ItemDataBound(Object s, RepeaterItemEventArgs e)
+        {
+            // This event is raised for the header, the footer, separators, and items.
+            // Execute the following logic for Items and Alternating Items.
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                LinkButton delbutton3 = (LinkButton)(e.Item.FindControl("delbutton3"));
+                HyperLink editbutton3 = (HyperLink)(e.Item.FindControl("editbutton3"));
+                delbutton3.Text = "<img border='0' src='../images/icon_delete.gif'>";
+                editbutton3.Text = "<img src='../images/icon_pencil.gif' alt='Edit' border='0'>";
+                editbutton3.NavigateUrl = "updatearticle.aspx?aid=" + DataBinder.Eval(e.Item.DataItem, "ID");
+                delbutton3.Attributes["onclick"] = "javascript:return confirm('Are you sure you want to delete " + DataBinder.Eval(e.Item.DataItem, "Title") + " Article, ID#  " + DataBinder.Eval(e.Item.DataItem, "ID") + "?')";
+                delbutton3.Attributes.Add("onmouseover", "Tip('Delete (<b>" + DataBinder.Eval(e.Item.DataItem, "Title") + "</b>) article.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                delbutton3.Attributes.Add("onmouseout", "UnTip()");
+                editbutton3.Attributes.Add("onmouseover", "Tip('Edit (<b>" + DataBinder.Eval(e.Item.DataItem, "Title") + "</b>) article.', BGCOLOR, '#FFFBE1', BORDERCOLOR, '#acc6db')");
+                editbutton3.Attributes.Add("onmouseout", "UnTip()");
+            }
+        }
+    }
+}
