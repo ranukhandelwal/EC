53a0ee1d99c5eb5b5bb868aeddfa0f09e8cd2f80
NKK <nkk@gmail.com>
2012-01-18 21:35:16 +0530

    Control: Discussion Form: Created
    Created discussion form control. Currently this is static control.
    Should be developed further.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ Control/DiscussionForm.ascx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Data;
+using System.Xml;
+using System.Text;
+using EC.Security;
+
+namespace ExamCrazy.Control
+{
+    public partial class DiscussionForm : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlLink csslink = new HtmlLink();
+            if (Page.Header.FindControl("cssdiscussion") == null)
+            {
+                csslink.ID = "cssdiscussion";
+                csslink.Attributes.Add("rel", "stylesheet");
+                csslink.Attributes.Add("type", "text/css");
+                csslink.Href = "CSS/discussion.css";
+                Page.Header.Controls.Add(csslink);
+            }
+
+            if (!Page.IsPostBack)
+            {
+                if(Authentication.Authenticate)
+                    discussionpanel.Visible = false;
+            }
+
+        }
+    }
+}
