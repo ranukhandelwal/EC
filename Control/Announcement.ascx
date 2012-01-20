5b1f32c75275d5dce88a1fdc94ab3458126a0ba5
NKK <nkk@gmail.com>
2012-01-20 18:11:08 +0530

    Control: Announcement: Added
    Added new control named announcement. This control will be used to show
    announcement in the website.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ Control/Announcement.ascx
@@ -0,0 +1,20 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Announcement.ascx.cs"
+    Inherits="ExamCrazy.Control.Announcement" %>
+<asp:Repeater ID="AnnounceListId" runat="server">
+    <HeaderTemplate>
+        <div class="announcement mod">
+            <h3>
+                <span class="unit">Announcements</span> <span class="arrows"></span>
+            </h3>
+            <div class="">
+                <ul>
+    </HeaderTemplate>
+    <ItemTemplate>
+        <li><%# Eval("Summary")%></li>
+            
+        
+    </ItemTemplate>
+    <FooterTemplate>
+        </ul> </div> </div>
+    </FooterTemplate>
+</asp:Repeater>
---
+++ Control/Announcement.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using EC.BL;
+using EC.Model;
+using EC.Common;
+
+namespace ExamCrazy.Control
+{
+    public partial class Announcement : System.Web.UI.UserControl
+    {
+        public int PAGEID = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HtmlLink csslink = new HtmlLink();
+            if (Page.Header.FindControl("cssannouncement") == null)
+            {
+                csslink.ID = "cssannouncement";
+                csslink.Attributes.Add("rel", "stylesheet");
+                csslink.Attributes.Add("type", "text/css");
+                csslink.Href = "CSS/announcement.css";
+                Page.Header.Controls.Add(csslink);
+            }
+            if (!IsPostBack)
+            {
+                AnnounceRepository AnnounceList = new AnnounceRepository();
+                ExtendedCollection<Announce> UC = AnnounceList.GetAnnounceList(PAGEID.ToString());
+                //UC.Contains(
+                Announce[] anouncements = new Announce[UC.Count];
+                UC.CopyTo(anouncements, 0);
+                AnnounceListId.DataSource = anouncements;
+                AnnounceListId.DataBind();
+            }
+
+        }
+    }
+}
