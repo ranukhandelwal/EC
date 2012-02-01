eb18790ac776c14977c3dbb5438cd360e43f0a22
NKK <nkk@gmail.com>
2012-02-01 21:48:45 +0530

    Login Verification: Base class
    All pages which require user login should be derived from this page. If
    user is not logged in then this class will prompt for login and then
    will move to correct page.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ App_Code/BaseLoggedInPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using EC.UI;
+using EC.Security;
+
+namespace EC.UI
+{
+    public class BaseLoggedInPage : BasePage
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            if (!Authentication.IsUserAuthenticated)
+            {
+                Response.Redirect(ResolveUrl("~/Test/TempIntermediatePage.aspx"));
+            }
+            base.OnLoad(e);
+        }        
+    }
+}
