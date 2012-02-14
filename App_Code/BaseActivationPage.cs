99891cf8ccf6d88a8e07bb04235ba0c3cfefd301
NKK <nkk@gmail.com>
2012-02-14 06:24:01 +0530

    Base Class: Base Activation Page
    Again in the heirarchy of BasePage classe added class for Base
    Activation.
    
    This class is used to verify Activation Links, e.g. email verification
    link.
    
    Take following example
    1) same page email-verify.aspx is used to generate email verification
    link and verify it
    2) Which particular functionality to be displayed depends on Base
    Activation class.
    
    If on load query strings corresponding to Activation are present then it
    alerts by setting Activation Variable that this is going to be
    Activation other wise it is transparent and works as BaseLogged in
    class.
    
    On receiving Activation Link this class verifies proper activation link
    and sets proper variable so that derived class can know if link was
    verified or not.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ App_Code/BaseActivationPage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EC.UI;
+using EC.Security;
+using EC.BL.Providers.User;
+using EC.Security;
+
+namespace EC.UI
+{
+    public class BaseActivationPage : BaseLoggedInPage
+    {
+        private string __domain;
+        private string __email;
+        private string __username;
+        private string __userid;
+
+        protected bool __isEmailLinkAuthenticate = false;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            __domain = Request.QueryString["class"];
+            __email = Request.QueryString["bl"];
+            __username = Request.QueryString["cl"];
+            __userid = Request.QueryString["sl"];
+            if (__domain != null && __email != null && __username != null && __userid != null)
+            {
+                base.Activation = true;
+                __domain = __domain.Replace(" ","+");
+                __email = __email.Replace(" ", "+");
+                __username = __username.Replace(" ", "+");
+                __userid = __userid.Replace(" ", "+");
+                UserActivationLink __verifyEmailLink = new UserActivationLink();
+                if (__verifyEmailLink.IsEmailLinkAuthenticate(Int32.Parse(Encryption.Decrypt(__userid)), Encryption.Decrypt(__username), Encryption.Decrypt(__email), Encryption.Decrypt(__domain)))
+                    __isEmailLinkAuthenticate = true;
+                else
+                    __isEmailLinkAuthenticate = false;
+
+            }
+            base.OnLoad(e);
+        }
+
+
+    }
+}
