f7f2f5e5db68653cccf1c3d9e784e75872a357bc
NKK <nkk@gmail.com>
2012-02-14 06:22:28 +0530

    User: User Activation Link: Generate Link and Validate Link
    Added class to generate and validate links.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ App_Code/BLL/Providers/User/UserActivationLink.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EC.Model;
+using EC.BL.Providers.User;
+using EC.Security;
+using EC.Common.Utilities;
+
+namespace EC.BL.Providers.User
+{
+    public class UserActivationLink : BaseUserObj
+    {
+        ProviderUserDetails user = new ProviderUserDetails();
+        private Utility Util
+        {
+            get { return new Utility(); }
+        }
+
+        public string GenerateActivationLink()
+        {
+            string link = "?";
+            if (Authentication.IsUserAuthenticated)
+            {
+                //user.FillUp(UserIdentity.UserID);
+                link = link + "class=" + Encryption.Encrypt(UserIdentity.UserEmail.Split('@')[1]);
+                link = link + "&";
+                link = link + "bl=" + Encryption.Encrypt(UserIdentity.UserEmail.Split('@')[0]);
+                link = link + "&";
+                link = link + "cl=" + Encryption.Encrypt(UserIdentity.UserName);
+                link = link + "&";
+                link = link + "sl=" + Encryption.Encrypt(UserIdentity.UserID.ToString());
+            }
+
+            return link;
+        }
+
+        public bool IsEmailLinkAuthenticate(int UserID, string UserName, string Email, string Domain)
+        {
+            if (Blogic.IsEmailLinkAuthenticate(UserID, Email + "@" + Domain, UserName) <= 0)
+                return false;
+            else
+                return true;
+
+        }
+
+
+    }
+}
