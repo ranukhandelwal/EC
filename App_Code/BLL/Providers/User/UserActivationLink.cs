7e1326c10cfbbc1627e6f8531f4d174be66f2b5e
NKK <nkk@gmail.com>
2012-02-17 19:21:31 +0530

    User: Activation Link: Support for forgot password
    Made following changes(also affected update email functions)
    
    1) In query string added action as a= vemail to verify as email
    2) Added function GeneratePasswordResetLink to generate link for forgot
    password
    3) This function user email as input and retreives user name and user id
    from it
    4) This function is generating link in same was as for verify email but
    some magic characters are added to make it different link
    5) action is pswd
    6) Another function doing just opposite of it, i.e. verifying link is
    added. This function is IsEmailLinkAuthenticate.
    
    Signed-off-by: NKK <nkk@gmail.com>

--- App_Code/BLL/Providers/User/UserActivationLink.cs
+++ App_Code/BLL/Providers/User/UserActivationLink.cs
@@ -30,11 +30,31 @@ namespace EC.BL.Providers.User
                 link = link + "cl=" + Encryption.Encrypt(UserIdentity.UserName);
                 link = link + "&";
                 link = link + "sl=" + Encryption.Encrypt(UserIdentity.UserID.ToString());
+                link = link + "&";
+                link = link + "a=vemail";
             }
 
             return link;
         }
 
+        public string GeneratePasswordResetLink(string Email)
+        {
+            string link = "?";
+            int UserId = Blogic.GetUserIDFromEmail(Email);
+            user.FillUp(UserId);
+            link = link + "class=" + Encryption.Encrypt("0" + user.Email1.Split('@')[1]);
+            link = link + "&";
+            link = link + "bl=" + Encryption.Encrypt("1" + user.Email1.Split('@')[0]);
+            link = link + "&";
+            link = link + "cl=" + Encryption.Encrypt("2" + user.UserName);
+            link = link + "&";
+            link = link + "sl=" + Encryption.Encrypt("3" + UserId.ToString());
+            link = link + "&";
+            link = link + "a=pswd";
+            
+            return link;
+        }
+       
         public bool IsEmailLinkAuthenticate(int UserID, string UserName, string Email, string Domain)
         {
             if (Blogic.IsEmailLinkAuthenticate(UserID, Email + "@" + Domain, UserName) <= 0)
@@ -44,6 +64,56 @@ namespace EC.BL.Providers.User
 
         }
 
+        public bool IsPasswordResetLinkAuthenticate(string _UserID, string _UserName, string _Email, string _Domain, out string UserName)
+        {
+            int UserID;
+            string Email;
+            string Domain;
+            
+            _UserID = Encryption.Decrypt(_UserID);
+            if (_UserID.Length > 1)
+            {
+                if (_UserID.Substring(0, 1) == "3")
+                {
+                    _UserID = _UserID.Substring(1, _UserID.Length - 1);
+                }
+            }
+            UserID = Int32.Parse(_UserID);
 
+            _UserName = Encryption.Decrypt(_UserName);
+            if (_UserName.Length > 1)
+            {
+                if (_UserName.Substring(0, 1) == "2")
+                {
+                    _UserName = _UserName.Substring(1, _UserName.Length - 1);
+                }
+            }
+            UserName = _UserName;
+
+            _Email = Encryption.Decrypt(_Email);
+            if (_Email.Length > 1)
+            {
+                if (_Email.Substring(0, 1) == "1")
+                {
+                    _Email = _Email.Substring(1, _Email.Length - 1);
+                }
+            }
+            Email = _Email;
+
+            _Domain = Encryption.Decrypt(_Domain);
+            if (_Domain.Length > 1)
+            {
+                if (_Domain.Substring(0, 1) == "0")
+                {
+                    _Domain = _Domain.Substring(1, _Domain.Length - 1);
+                }
+            }
+            Domain = _Domain;
+            if (Blogic.IsEmailLinkAuthenticate(UserID, Email + "@" + Domain, UserName) <= 0)
+                return false;
+            else
+                return true;
+
+        }
     }
 }
