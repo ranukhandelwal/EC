29a1250e53d160c09834f56ede2d0b4a7711722c
NKK <nkk@gmail.com>
2012-02-18 11:39:57 +0530

    Providers: Login: Added provider for login/logout
    Earlier login and logout was only with login control. But login logout
    can be required even without this control. E.g. with change of password
    user is logged out.
    
    This provides should be used for all login logout related activities.
    
    Signed-off-by: NKK <nkk@gmail.com>

---
+++ App_Code/BLL/Providers/Login/LoginRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EC.UI;
+using EC.BL;
+using EC.Common;
+using EC.Security;
+using EC.BL.Providers.User;
+
+namespace EC.BL
+{
+    public static class LoginRepository
+    {
+        public static int Login(string uname, string upass)
+        {
+            //Autheticate the username and password against the database record.
+            if (Authentication.Validate(uname, Encryption.Encrypt(upass)))
+            {
+                
+                /*
+                if (!Blogic.IsUserActive(uname, Encryption.Encrypt(upass)))
+                {
+                    CookieLoginHelper.RemoveCookie();
+                    CookieLoginHelper.RemoveLoginSession();
+
+                    this.Context.Response.Redirect("redirectionpage.aspx?mode=suspended&ReturnURL=" + this.Context.Request.Url.PathAndQuery);
+                }
+                 */
+
+                if ((CookieLoginHelper.IsCookieSupported))
+                {
+                    CookieLoginHelper.CreateLoginCookie(uname, upass);
+                }
+                else
+                {
+                    //If the users browser does not support cookie, use session instead.
+                    CookieLoginHelper.CreateLoginSession(uname, upass);
+                }
+
+                HttpContext.Current.Response.Redirect(SiteConfiguration.LoginRedirectPage());
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+
+        public static void Logout()
+        {
+            CookieLoginHelper.RemoveCookie();
+            CookieLoginHelper.RemoveLoginSession();
+
+            HttpContext.Current.Response.Redirect("~");
+        }
+    }
+}
