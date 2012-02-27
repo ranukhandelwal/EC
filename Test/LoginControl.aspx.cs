346ea70c6a884aad1d03ba10c0d3986000fc99b9
NKK <nkk@gmail.com>
2012-02-27 21:56:46 +0530

    Test: Login Control: Updated page for ModalPopup login
    Updated test page for Modal Popup login.
    
    Signed-off-by: NKK <nkk@gmail.com>

--- Test/LoginControl.aspx.cs
+++ Test/LoginControl.aspx.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using EC.UI;
 
 namespace ExamCrazy.Test
 {
-    public partial class LoginControl : System.Web.UI.Page
+    public partial class LoginControl : BaseLoggedInPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            login.ShowPopup();
 
         }
     }
