720e50e240fb747dd151e0f92b7c4bd6b2a93d3c
NKK <nkk@gmail.com>
2012-02-27 21:54:09 +0530

    Master: Header Footer: Script manager call
    Added support of script manager in header footer master.
    
    Signed-off-by: NKK <nkk@gmail.com>

--- HeaderFooter.Master.designer.cs
+++ HeaderFooter.Master.designer.cs
@@ -31,6 +31,15 @@ namespace ExamCrazy {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ToolkitScriptManager ScriptManager1;
+        
+        /// <summary>
         /// login control.
         /// </summary>
         /// <remarks>
